05b609a4775d36fa64cc8c3347e79445ef4c6a66
Greg Detre <greg@gregdetre.co.uk>
2015-10-13 00:49:18 -0400

    Cosmetic tweaks to tests

--- test_legal.py
+++ test_legal.py
@@ -3,11 +3,10 @@ from isitanemail import isitanemail
 # USAGE:
 # $ nose2 test_legal
 # $ nose2 test_legal.test_legal_basic
-# $ python test_legal.py
 
 
 def test_legal_basic():
-    assert isitanemail('ab@de.com')
+    assert isitanemail('ab@cd.com')
 
 
 
